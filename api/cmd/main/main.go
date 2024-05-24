1dba1a7cb712353a564f17d3cb56a3bcaa1b5144
Nikita Khvorostin <khvorostinn@gmail.com>
2024-05-24 15:50:39 +0300

    initalize db connection at main.go

--- api/cmd/main/main.go
+++ api/cmd/main/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/nitishm/go-rejson/v4"
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
 	fmt.Println("http://localhost:5000")
+
+	ctx := context.Background()
+	rh := rejson.NewReJSONHandler()
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   0,
+	})
+
+	rh.SetGoRedisClientWithContext(ctx, rdb)
+
 	e := echo.New()
 	e.Use(middleware.CORS())
+
 	e.Logger.Fatal(e.Start(":5000"))
 }
